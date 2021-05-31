1388ca6be645cfe40fb9abd9bd2102cf8893293d
FinnHeu <finn.heukamp@t-online.de>
2021-05-31 10:18:28 +0200

    add files

---
+++ build/lib/python_utils/plotting.py
@@ -0,0 +1,128 @@
+import matplotlib.pyplot as plt
+import cartopy.crs as ccrs
+import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
+import cmocean.cm as cmo
+import cartopy.crs as ccrs
+import cartopy.feature as cfeature
+from cartopy.mpl.gridliner import LONGITUDE_FORMATTER, LATITUDE_FORMATTER
+
+
+def plot_background(ax, extent="BS", land=True, coastline=True):
+
+    '''
+    plot background.py
+
+    Plots background plot and adds cartopy features for specific regions.
+
+    Inputs
+    ------
+    ax: cartopy geoaxis
+    extent: 'BS', 'Arc', 'Arc+', [lon lon, lat lat]
+    land=True
+    coastline=True
+
+    Returns
+    -------
+    ax
+
+
+    Usage
+    -------
+    fig, ax = plt.subplots(1,1, figsize=(10,7.5), subplot_kw=dict(projection=ccrs.NorthPolarStereo()))
+    plot_background(ax, extent='BS', land=False)
+    '''
+
+
+    if extent == "BS":
+
+        gl = ax.gridlines(
+        crs=ccrs.PlateCarree(),
+        draw_labels=True,
+        linewidth=1,
+        color="gray",
+        alpha=0.5,
+        linestyle="--",
+        x_inline=True,
+        y_inline=False,
+        zorder=20,
+    )
+        ax.set_extent([0, 90, 66, 90], crs=ccrs.PlateCarree())
+
+        gl.xlocator = mticker.FixedLocator(
+            [20, 40, 60, 80],
+        )
+        gl.ylocator = mticker.FixedLocator(np.arange(70, 90, 5))
+        gl.xformatter = LONGITUDE_FORMATTER
+        gl.yformatter = LATITUDE_FORMATTER
+
+    elif extent == "Arc+":
+
+        gl = ax.gridlines(
+        crs=ccrs.PlateCarree(),
+        draw_labels=True,
+        linewidth=1,
+        color="gray",
+        alpha=0.5,
+        linestyle="--",
+        x_inline=False,
+        y_inline=True,
+        zorder=20,
+    )
+        ax.set_extent([-180, 180, 45, 90], crs=ccrs.PlateCarree())
+
+        gl.xlocator = mticker.FixedLocator(np.arange(-180, 180, 30))
+        gl.ylocator = mticker.FixedLocator(np.arange(45, 90, 5))
+        gl.xformatter = LONGITUDE_FORMATTER
+        gl.yformatter = LATITUDE_FORMATTER
+
+    elif extent == "Arc":
+
+        gl = ax.gridlines(
+        crs=ccrs.PlateCarree(),
+        draw_labels=True,
+        linewidth=1,
+        color="gray",
+        alpha=0.5,
+        linestyle="--",
+        x_inline=False,
+        y_inline=True,
+        zorder=20,
+    )
+        ax.set_extent([-180, 180, 60, 90], crs=ccrs.PlateCarree())
+
+        gl.xlocator = mticker.FixedLocator(np.arange(-180, 180, 30))
+        gl.ylocator = mticker.FixedLocator(np.arange(55, 90, 5))
+        gl.xformatter = LONGITUDE_FORMATTER
+        gl.yformatter = LATITUDE_FORMATTER
+
+    else:
+        gl = ax.gridlines(
+        crs=ccrs.PlateCarree(),
+        draw_labels=True,
+        linewidth=1,
+        color="gray",
+        alpha=0.5,
+        linestyle="--",
+        x_inline=False,
+        y_inline=True,
+        zorder=20,
+    )
+        ax.set_extent(extent, crs=ccrs.PlateCarree())
+
+        gl.xlocator = mticker.FixedLocator(np.arange(-180, 180, 30))
+        gl.ylocator = mticker.FixedLocator(np.arange(35, 90, 5))
+        gl.xformatter = LONGITUDE_FORMATTER
+        gl.yformatter = LATITUDE_FORMATTER
+
+
+    if land:
+        ax.add_feature(cfeature.LAND, zorder=10)
+    if coastline:
+        ax.add_feature(cfeature.COASTLINE, zorder=10)
+
+    gl.xlabel_style = {"size": 12, "rotation": 0}
+    gl.ylabel_style = {"size": 12, "rotation": 0}
+
+
+    return ax
