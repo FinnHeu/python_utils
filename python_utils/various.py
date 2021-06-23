34b2461ae0ebb6006f2d1936b2838c44adf32ad2
FinnHeu <Finn.Heukamp@t-online.de>
2021-06-23 13:21:30 +0200

    Added wind deflection for surface pressure fields

--- python_utils/various.py
+++ python_utils/various.py
@@ -1,37 +1,38 @@
 import numpy as np
 
-def slp_to_geowind(slp, lon, lat, rho = 1.225, Re = 6371000, f='f-plane', f_central=None):
+def pressure_to_geowind(pressure, lon, lat, rho = 1.225, Re = 6371000, f='f-plane', f_central=None, slp=False):
 
     '''
-    slp_to_geowind.py
+    pressure_to_geowind.py
 
     Computes geostrophic winds from sea level pressure field
 
     Inputs
     ------
-    slp: np.ndarray, sea level pressure field
+    pressure: np.ndarray, sea level pressure field
     lon: np.ndarray, longitude vector
     lat: np.ndarray, latitude vector
     rho = 1.225, air density
     Re = 6371000, radius earth
     f='f-plane' or 'real_world'
     f_central=None, central latitude for f-plane
+    slp = False, correct for surface friction (Protushinsky and Johnson 1997) when using sea level pressure fields
 
     Returns
     -------
     u, v: zonal and meridional wind components
     '''
-    
+
     # Compute the distances between the gridpoints for computing the gradient
     dist_x = np.diff(lon)[0] * (2 * np.pi * Re / 360) * np.cos(lat/360 * 2 * np.pi)
     dist_y = np.diff(lat) * (2 * np.pi * Re / 360)
 
     # Compute the gradient
-    dpdx = np.ones_like(slp) * np.nan
-    dpdy = np.ones_like(slp) * np.nan
+    dpdx = np.ones_like(pressure) * np.nan
+    dpdy = np.ones_like(pressure) * np.nan
 
-    for i in range(len(lat)): dpdx[i,:] = np.gradient(slp[i,:], dist_x[i])
-    for i in range(len(lon)): dpdy[:,i] = np.gradient(slp[:,i], np.mean(dist_y))
+    for i in range(len(lat)): dpdx[i,:] = np.gradient(pressure[i,:], dist_x[i])
+    for i in range(len(lon)): dpdy[:,i] = np.gradient(pressure[:,i], np.mean(dist_y))
 
     # Compute Coriolis parameter
     if f == 'f-plane':
@@ -47,5 +48,24 @@ def slp_to_geowind(slp, lon, lat, rho = 1.225, Re = 6371000, f='f-plane', f_cent
     # Compute geostrophic velocity
     u = 1/(rho * f_c) * dpdy
     v = -1/(rho * f_c) * dpdx
+
+    if slp:
+        # Deflect and reduce winds for the impact of surface friction (Protushinsky and Johnson 1997)
+        rad = np.pi/180
+        scale = .7
+        angle = 30
+
+        rot_mat = np.array([[np.cos(angle*rad), -np.sin(angle*rad)],[np.sin(angle*rad), np.cos(angle*rad)]])
+
+        a = u.shape
+        for i in range(a[0]):
+            for j in range(a[1]):
+
+                vec = np.array([u[i,j], v[i,j]]) # extract velocity vector
+                rot_vec = scale * np.dot(rot_mat, vec) #  rotate and scale
+
+                u[i,j] = rot_vec[0]
+                v[i,j] = rot_vec[1]
+
 
     return u, v
