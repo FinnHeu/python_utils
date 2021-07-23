28a195844116b0b3038006d0ae056e4efd0d45b2
FinnHeu <Finn.Heukamp@t-online.de>
2021-07-23 10:23:46 +0200

    Added new var name for slp

--- python_utils/dataset_operations.py
+++ python_utils/dataset_operations.py
@@ -3,10 +3,7 @@ import xarray as xr
 import glob
 
 
-
-
 def dataset_to_cfconvention(ds, longitude='lon', latitude='lat', time='time', slp='slp', temp='temp', salt='salt', lon180=True):
-
     '''
     dataset_to_cfconvention.py
 
@@ -32,7 +29,7 @@ def dataset_to_cfconvention(ds, longitude='lon', latitude='lat', time='time', sl
 
     # Rename Coordinates
 
-    time_names = ['TIME','Time']
+    time_names = ['TIME', 'Time']
     lon_names = ['LONGITUDE', 'Longitude', 'longitude', 'LON', 'Lon', 'LONS', 'lons', 'Lons']
     lat_names = ['LATITUDE', 'Latitude', 'latitude', 'LAT', 'Lat', 'LATS', 'lats', 'Lats']
 
@@ -51,10 +48,10 @@ def dataset_to_cfconvention(ds, longitude='lon', latitude='lat', time='time', sl
             if (coord_name == lat_name):
                 ds = ds.rename({coord_name: latitude})
 
-
     # Rename Data variables
 
-    slp_names = ['SLP', 'Slp', 'PSL', 'psl', 'Psl', 'sea_level_pressure', 'Sea_Level_Pressure']
+    slp_names = ['SLP', 'Slp', 'PSL', 'psl', 'Psl',
+                 'sea_level_pressure', 'Sea_Level_Pressure', 'msl']
 
     data = list(ds.keys())
 
@@ -64,7 +61,6 @@ def dataset_to_cfconvention(ds, longitude='lon', latitude='lat', time='time', sl
             if (data_name == slp_name):
                 ds = ds.rename({data_name: slp})
 
-
     # Covert 0-360°E to -180 - 180°E
     if lon180:
         ds.coords['lon'] = (ds.coords['lon'] + 180) % 360 - 180
@@ -72,7 +68,8 @@ def dataset_to_cfconvention(ds, longitude='lon', latitude='lat', time='time', sl
 
     # Rename Data variables
 
-    temp_names = ['Temp', 'TEMP', 'tas', 'T_2_MOD','T_10_MOD', 'temperature', 'Temperature', 'TEMPERATURE']
+    temp_names = ['Temp', 'TEMP', 'tas', 'T_2_MOD', 'T_10_MOD',
+                  'temperature', 'Temperature', 'TEMPERATURE']
 
     data = list(ds.keys())
 
@@ -81,7 +78,6 @@ def dataset_to_cfconvention(ds, longitude='lon', latitude='lat', time='time', sl
         for temp_name in temp_names:
             if (data_name == temp_name):
                 ds = ds.rename({data_name: temp})
-
 
     # Covert 0-360°E to -180 - 180°E
     if lon180:
@@ -94,8 +90,7 @@ def dataset_to_cfconvention(ds, longitude='lon', latitude='lat', time='time', sl
     return ds
 
 
-def select_winter_month(ds, month=[12,1,2,3,4,5], mean=False):
-
+def select_winter_month(ds, month=[12, 1, 2, 3, 4, 5], mean=False):
     '''
     select_winter_month.py
 
@@ -111,7 +106,7 @@ def select_winter_month(ds, month=[12,1,2,3,4,5], mean=False):
         apply time mean
 
     '''
-    
+
     # Apply cf conventions if nesseccary
     if not 'time' in list(ds.coords):
         ds = dataset_to_cfconvention(ds)
@@ -131,3 +126,46 @@ def select_winter_month(ds, month=[12,1,2,3,4,5], mean=False):
         ds = ds.mean(dim='time')
 
     return ds
+
+
+def crop_fesom_to_region(src, mesh, dest, region=[0, 90, 50, 90]):
+    '''crop_fesom_to_region.py
+
+    Crops fesom output to a specific region to save memory
+
+    Inputs
+    ------
+    src (str)
+        paths to files to process
+    mesh (fesom.mesh)
+        fesom mesh object
+    dest (str)
+        destination path to save output
+    region (list)
+        list of form [lon1 lon2 lat1 lat2] defining the box
+
+    Returns
+    -------
+
+    '''
+    # handle strings and list of strings
+    if isinstance(src, str):
+        src = [src]
+    elif isinstance(src, list):
+        pass
+    elif isinstance(src, np.ndarray):
+        pass
+    else:
+        raise ValueError('data type not supported')
+
+    # compute indices to crop
+    inds = np.where((mesh.x2 >= region[0]) & (mesh.x2 <= region[1]) & (
+        mesh.y2 >= region[2]) & (mesh.y2 <= region[3]))[0]
+
+    # load individual files, crop and save
+    for i in tqdm(range(len(src))):
+
+        ds = xr.open_dataset(src[i], chunks={'nod2': 1e4}).isel(nod2=inds).load()
+        ds.to_netcdf(dest[i])
+
+    return inds
