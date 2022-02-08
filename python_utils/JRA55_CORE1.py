8381c6c3ca48de371e61cf1420e5b6fd69d9d448
FinnHeu <Finn.Heukamp@t-online.de>
2022-02-08 11:52:54 +0100

    Added regression analysis

--- python_utils/JRA55_CORE1.py
+++ python_utils/JRA55_CORE1.py
@@ -1,6 +1,7 @@
 import xarray as xr
 import pandas as pd
 import numpy as np
+from sklearn.linear_model import LinearRegression
 from eofs.xarray import Eof
 from .dataset_operations import select_winter_month, dataset_to_cfconvention
 
@@ -80,6 +81,126 @@ def NAOindex(src_path: str, newdates: tuple, slpvar='psl', timeslice=('1960','20
     return eofs, pcs
 
 
+def multiple_linear_regression_reanalysis(dependent_var, independent_var1, independent_var2, var_names=['uas', 'vas']):
+    ''''''
+    # process dependent data
+    dep_var = dependent_var.values
+
+    array_size = independent_var1[var_names[0]].shape
+    for ii in tqdm(range(array_size[1])):
+        for jj in range(array_size[2]):
+
+            # cut single grid point
+            temp_indep_var1 = independent_var1[var_names[0]].values[:, ii, jj]
+            temp_indep_var2 = independent_var2[var_names[1]].values[:, ii, jj]
+
+            # bring the independent data into the right shape
+            indep_vars = np.empty((len(temp_indep_var1), 2))
+            for i in range(len(temp_indep_var1)):
+                indep_vars[i, 0] = temp_indep_var1[i]
+                indep_vars[i, 1] = temp_indep_var2[i]
+
+            # initialize the final arrays in the very first loop
+            if (ii == 0) & (jj == 0):
+                R2_prediction_full = np.empty((array_size[1], array_size[2]))
+                intercepts_full = np.empty((array_size[1], array_size[2]))
+                predicted_var_full = np.empty((len(dep_var), array_size[1], array_size[2]))
+                coeffs_full = np.empty((2, array_size[1], array_size[2]))
+
+            ##### perform regression fit for whole data
+            lr = LinearRegression()
+            lr.fit(indep_vars, dep_var)
+            # compute R^2 between
+            R2_prediction_full[ii, jj] = lr.score(indep_vars, dep_var)
+            predicted_var_full[:, ii, jj] = lr.predict(indep_vars)
+            intercepts_full[ii, jj] = lr.intercept_
+            coeffs_full[:, ii, jj] = lr.coef_
+
+    return predicted_var_full, R2_prediction_full, coeffs_full, intercepts_full, predicted_var_full
+
+
+def linear_regression_reanalysis(dependent_var, independent_var1, var_names='psl'):
+    ''''''
+    # process dependent data
+    dep_var = dependent_var.values
+
+    array_size = independent_var1[var_names].shape
+    for ii in tqdm(range(array_size[1])):
+        for jj in range(array_size[2]):
+
+            # cut single grid point
+            temp_indep_var1 = independent_var1[var_names].values[:, ii, jj]
+
+            # bring the independent data into the right shape
+            indep_vars = np.empty((len(temp_indep_var1), 1))
+            for i in range(len(temp_indep_var1)):
+                indep_vars[i, 0] = temp_indep_var1[i]
+
+            # initialize the final arrays in the very first loop
+            if (ii == 0) & (jj == 0):
+                R2_prediction_full = np.empty((array_size[1], array_size[2]))
+                intercepts_full = np.empty((array_size[1], array_size[2]))
+                predicted_var_full = np.empty((len(dep_var), array_size[1], array_size[2]))
+                coeffs_full = np.empty((2, array_size[1], array_size[2]))
+
+            ##### perform regression fit for whole data
+            lr = LinearRegression()
+            lr.fit(indep_vars, dep_var)
+            # compute R^2 between
+            R2_prediction_full[ii, jj] = lr.score(indep_vars, dep_var)
+            predicted_var_full[:, ii, jj] = lr.predict(indep_vars)
+            intercepts_full[ii, jj] = lr.intercept_
+            coeffs_full[:, ii, jj] = lr.coef_
+
+    return predicted_var_full, R2_prediction_full, coeffs_full, intercepts_full
+
+
+def CorrelationReanalysis(var, field, variable='psl'):
+    field = field[variable].values
+
+    array_size = field.shape
+    # preallocate array
+    corrcoeffs = np.empty((array_size[1], array_size[2]))
+    p_values = np.empty((array_size[1], array_size[2]))
+
+    for ii in tqdm(range(array_size[1])):
+        for jj in range(array_size[2]):
+            corrcoeffs[ii, jj], p_values[ii, jj] = pearsonr(var, field[:, ii, jj])
+
+    return corrcoeffs, p_values
+
+
+def CorrCoeffRegressOri(p_rea_full, vol_transport):
+    '''
+    Computes the Pearson Correlation Coefficient between the reconstructed timeseries (from regression) and the original timeseries
+    '''
+
+    # preallocate arrays
+    array_shape = p_rea_full.shape
+    corrcoeff = np.empty((array_shape[1], array_shape[2]))
+    p_val = np.empty((array_shape[1], array_shape[2]))
+
+    # compute correlation coeff.
+    for ii in tqdm(range(array_shape[1])):
+        for jj in range(array_shape[2]):
+            corrcoeff[ii, jj], p_val[ii, jj] = pearsonr(vol_transport.values, p_rea_full[:, ii, jj])
+
+    return corrcoeff, p_val
+
+
+def PreferredWindDir(c_rea, corrcoeff, p_val, min_correlation=.5, p_val_max=.05):
+    '''
+    Computes the Preferred Wind Direction of a multi parameter (u,v) wind field regression
+    '''
+
+    # Compute the vectors
+    wind_vectors = c_rea / np.sqrt(c_rea[0, :, :] ** 2 + c_rea[1, :, :] ** 2)[np.newaxis, :]
+
+    # Optional mask
+    mask = np.where((corrcoeff < min_correlation) | (p_val > p_val_max), np.nan, 1)
+    wind_vectors = wind_vectors * mask[np.newaxis, :]
+
+    return wind_vectors
 
 
 
