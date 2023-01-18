4bde124c4c04bf96d027723f348bd8a1aa41bce0
Finn Heukamp <57488986+FinnHeu@users.noreply.github.com>
2023-01-18 13:28:22 +0100

    Add al functions from ClimateModes

--- python_utils/__init__.py
+++ python_utils/__init__.py
@@ -4,7 +4,7 @@ from .plotting import *
 from .dataset_operations import *
 from .bs_anomalies import *
 from .JRA55_CORE1 import *
-from .ClimateModes import NAMindex, NAOindex, BOindex, ADindex
+from .ClimateModes import * #NAMindex, NAOindex, BOindex, ADindex
 from .statistics import *
 from .ice import *
 from .BarentsSeaAtlanticWater import *
